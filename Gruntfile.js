df51974145de428d424705953f05aa1083be85ad
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-04-14 14:16:50 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.util.linefeed = '\n';
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+              ' * Bootstrap HTML v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+              ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+              ' */\n\n',
+
+    clean: {
+      dist: ['dist/']
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: false
+      },
+      dist: {
+        src: [
+          'bower_components/bootstrap/dist/js/bootstrap.js',
+          'bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js',
+          'bower_components/bootstrap-select/bootstrap-select.js',
+          'bower_components/bootstrap-spinner/dist/bootstrap-spinner.js',
+          'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+          'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js'
+        ],
+        dest: 'dist/assets/js/bootstrap.js'
+      }
+    },
+
+    less: {
+      dist: {
+        files: {
+          'dist/assets/css/style.css': 'less/style.less'
+        }
+      }
+    },
+
+    copy: {
+      js: {
+        src: 'js/*',
+        dest: 'dist/assets/'
+      },
+      vendor_js: {
+        expand: true,
+        flatten: true,
+        src: [
+          'bower_components/jquery/jquery.min.js',
+          'bower_components/jquery-bootswatch/jquery.bootswatch.js'
+        ],
+        dest: 'dist/assets/js/lib/'
+      }
+    },
+
+    processhtml: {
+      options: {
+        recursive: true
+      },
+      dist: {
+        files:[
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/*.html'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
+
+    jsbeautifier: {
+      files : ["dist/*.html"],
+    },
+
+    watch: {
+      options: { livereload: true },
+      html: {
+        files: 'src/**/*.html',
+        tasks: ['html']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          livereload: true,
+          port: 4000,
+          base: 'dist',
+          hostname: '*',
+        }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('html', ['processhtml', 'jsbeautifier']);
+
+  grunt.registerTask('dist', ['clean','copy', 'concat', 'less', 'html']);
+
+  grunt.registerTask('default', ['connect', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+# Bootstrap HTML
+
+**Bootstrap HTML files to be built with** `grunt-processhtml`
+
+Check how popular bootstrap plugins will look like with your theme. Provide your theme with example HTML files
+
+Included plugins
+* [Bootstrap Datepicker](https://github.com/eternicode/bootstrap-datepicker/)
+
+
+## Install
+
+Via Bower
+
+``` bash
+bower install bootstrap-html
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](https://github.com/sagikazarmark/bootstrap-html/blob/develop/LICENSE) for more information.
