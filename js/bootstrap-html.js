bcd585b8ff0ba8f2be46a36b272a49e30c307b11
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-04-14 20:25:12 +0200

    Added plugins

--- js/bootstrap-html.js
+++ js/bootstrap-html.js
@@ -2,5 +2,8 @@
   $(document).ready(function($) {
     $('[data-toggle="popover"]').popover();
     $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="switch"]').bootstrapSwitch();
+    $('[data-toggle="tag"]').tagsinput();
+    $('.select').selectpicker();
   });
 })(jQuery);
